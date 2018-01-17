54ac2dad189e79a0496bc92b6429003ff22ba6db
John Beech <johnbeech.github@mkv25.net>
2018-01-17 06:53:17 +0000

    Rename index to fetch-collection.js, stub out index.js

---
+++ fetch-collection.js
@@ -0,0 +1,23 @@
+const { fetch, write } = require('promise-path')
+const convert = require('xml-js')
+const username = process.env.USERNAME || 'hannardynamite'
+
+console.log('Stubbed fetcher')
+
+const collectionUrl = `https://www.boardgamegeek.com/xmlapi2/collection?username=${username}`
+
+console.log('[Fetch Collection]', collectionUrl)
+fetch(collectionUrl)
+  .then(response => {
+    console.log('[Raw Fetch]', response)
+    const result = convert.xml2js(response, {compact: true, alwaysArray: true, ignoreDeclaration: true, nativeType: true})
+    console.log('[JSON]', JSON.stringify(result, null, 2))
+    return result
+  })
+  .then(collection => {
+    const body = JSON.stringify(collection, null, 2)
+    write('collection.json', body, 'utf8')
+  })
+  .catch(ex => {
+    console.error('[Fetch Error]', ex, ex.stack)
+  })
