bf6914eb9db24088da0b297f0a2813aabdff3f7c
Hannah Bunce <hannahlbunce@gmail.com>
2018-01-21 20:46:12 +0000

    Create list of board game names from collection.json using a map function

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 console.log('Stubbed entrypoint...')
 console.log('  To regenerate collection.json, run:')
 console.log('  node fetch-collection.js')
+
+const collection = require('./collection.json')
+const names = collection.items[0].item.map(returnBoardGameName)
+
+function returnBoardGameName(item) {
+  return item.name[0]._text[0]
+}
+console.log(names)
