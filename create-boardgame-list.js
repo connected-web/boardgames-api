c7813959e81f73840d2b321d7a34b1572e33ba96
Hannah Bunce <hannahlbunce@gmail.com>
2018-01-21 21:06:54 +0000

    Fix #2 - Pick random game from list of board game names

--- create-boardgame-list.js
+++ create-boardgame-list.js
@@ -3,7 +3,7 @@ const { write } = require('promise-path')
 const collection = require('./collection.json')
 const boardGameNames = collection.items[0].item.map(returnBoardGameName)
 
-function returnBoardGameName(item) {
+function returnBoardGameName (item) {
   return item.name[0]._text[0]
 }
 console.log(boardGameNames)
--- index.js
+++ index.js
@@ -1,3 +1,6 @@
-console.log('Stubbed entrypoint...')
-console.log('  To regenerate collection.json, run:')
-console.log('  node fetch-collection.js')
+const boardGameNames = require('./boardgame-names.json')
+
+const randomIndex = Math.floor(Math.random() * boardGameNames.length)
+const randomBoardGameName = boardGameNames[randomIndex]
+
+console.log('Random pick:', randomIndex, 'of', boardGameNames.length, 'picked', randomBoardGameName)
