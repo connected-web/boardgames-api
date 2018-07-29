b66fc2c66d8e348e67b1499c64adce028785ce59
Hannah Bunce <hannahlbunce@gmail.com>
2018-07-29 08:55:13 +0100

    Add way to map boardgame names to IDs

---
+++ create-boardgame-index.js
@@ -0,0 +1,14 @@
+const { write } = require('promise-path')
+
+const collection = require('./collection.json')
+const boardGameIndex = collection.items[0].item.reduce(mapBoardGame, {})
+
+function mapBoardGame (accumulator, item) {
+   const name = item.name[0]._text[0]
+   const id = item._attributes.objectid
+   accumulator[name] = id
+   return accumulator
+}
+console.log(boardGameIndex)
+
+write('./boardgame-index.json', JSON.stringify(boardGameIndex, null, 2), 'utf8')
