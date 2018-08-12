c3b266b92468eca72e7ac96b204a3bf97f363a7f
John Beech <johnbeech.github@mkv25.net>
2018-08-12 19:19:45 +0100

    Compare the board game geek games list with Cali's local list

--- create-boardgame-list.js
+++ create-boardgame-list.js
@@ -1,12 +1,29 @@
 const { write } = require('promise-path')
 
 const bggCollection = require('./bgg-collection.json')
-const boardGameNames = bggCollection.items[0].item.map(returnBoardGameName)
+const caliCollection = require('./cali-boardgames.json')
 
-function returnBoardGameName (item) {
-  return item.name[0]._text[0]
+const bggBoardGameNames = bggCollection.items[0].item.map(item => item.name[0]._text[0])
+const caliBoardGameNames = Array.from(new Set(caliCollection.map(item => item.game)))
+
+const overlap = bggBoardGameNames.filter(n => caliBoardGameNames.includes(n))
+const bggOnly = bggBoardGameNames.filter(n => !caliBoardGameNames.includes(n))
+const caliOnly = caliBoardGameNames.filter(n => !bggBoardGameNames.includes(n))
+
+const stats = {
+  'Number of Board Game Geek board games': bggBoardGameNames.length,
+  'Number of Cali board games': caliBoardGameNames.length,
+  'Overlap size between lists': overlap.length,
+  'Board Game Geek only games': bggOnly.length,
+  'Cali only games': caliOnly.length,
 }
-console.log(boardGameNames)
 
+caliCollection.forEach(item => {
+  if (!item.game) {
+    console.log('Bad data on', item, new Date(1900, 0, item.date))
+  }
+})
 
-write('./boardgame-names.json', JSON.stringify(boardGameNames, null, 2), 'utf8')
+console.log('Board Game name stats:', stats)
+
+write('./boardgame-names.json', JSON.stringify({boardGameGeek: bggBoardGameNames, cali: caliBoardGameNames, overlap, stats}, null, 2), 'utf8')
