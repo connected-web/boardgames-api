3f7e117da26258fa1bbad5ed7d60eaf49f7461ec
John Beech <johnbeech.github@mkv25.net>
2018-08-12 18:09:06 +0100

    Collapse worksheets down into single list

--- download-gsheets-data.js
+++ download-gsheets-data.js
@@ -3,12 +3,13 @@ const { write } = require('promise-path')
 
 async function downloadData(spreadsheetId) {
   try {
-    const data = await  gsjson({ spreadsheetId, allWorksheets: true })
-    console.log('Downloaded data:', data.length, 'bytes')
-    write('cali-boardgames.json', JSON.stringify(data, null, 2), 'utf8')
+    const worksheets = await gsjson({ spreadsheetId, allWorksheets: true })
+    console.log('Downloaded data:', (worksheets + '').length, 'bytes')
+    const entries = worksheets.reduce((acc, item) => acc.concat(item), [])
+    write('cali-boardgames.json', JSON.stringify(entries, null, 2), 'utf8')
   }
   catch(ex) {
-    console.error('Unable download data', ex.message)
+    console.error('Unable to download data:', ex)
   }
 }
 
