7cc6130e701e8d8c74ad38018b5ccfaf37673b02
John Beech <johnbeech.github@mkv25.net>
2018-09-23 21:28:30 +0100

    Create the basic template for a docs page

--- docs/index.php
+++ docs/index.php
@@ -11,12 +11,21 @@ $requestPath = join(array_filter(explode('/', $requestUri)), '/');
 $templateHTML = @file_get_contents('./template.html');
 
 $site = 'Boardgame API Docs';
-$title = strip_tags('Home - ' . $requestPath);
+$title = 'Home';
 
 $outputHTML = $templateHTML;
-$outputHTML = str_replace('{{title}}', "$title - $site", $outputHTML);
-$outputHTML = str_replace('{{navigation}}', '<p>Navigation</p>', $outputHTML);
-$outputHTML = str_replace('{{body}}', '<p>Content</p>', $outputHTML);
-$outputHTML = str_replace('{{footer}}', '<p>Footer</p>', $outputHTML);
+$outputHTML = str_replace('{{title}}', "$title - $requestPath - $site", $outputHTML);
+$outputHTML = str_replace('{{navigation}}', '<nav><heading>{{icon:bars}} Navigation</heading><p>No where to navigate to yet.</p></nav>', $outputHTML);
+$outputHTML = str_replace('{{body}}', '<content><heading>{{icon:home}} ' . $title . '</heading><p>These docs are for the boardgames API. Where possible they will be kept in sync with the avaialble endpoints and will provide suitable documentation, schemas, and examples.</content>', $outputHTML);
+$outputHTML = str_replace('{{footer}}', '<footer><heading>{{icon:shoe-prints}} Footer</heading><p>The Board Game API is part of <a href="https://calisaurus.net">{{icon:home}} calisaurus.net</a> in support of <a href="https://boardgames.calisaurus.net">{{icon:dice}} boardgames.calisaurus.net blog</a> and associated apps, and has been built in association with <a href="https://github.com/connected-web/">{{icon:github:b}} Connected Web</a>.</footer>', $outputHTML);
+$outputHTML = renderIconsAsHTML($outputHTML);
+
+function renderIconsAsHTML($searchText) {
+  return str_replace('fa ', 'fas ', preg_replace(
+      '/({{icon:([a-z-]+):?([srlb])?}})/',
+      '<i class="fa$3 fa-$2"></i>',
+      $searchText
+  ));
+}
 
 print $outputHTML;
