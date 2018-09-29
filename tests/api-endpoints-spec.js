e3119d8012baba9e082ff4f75c15338c5cb1cd12
John Beech <johnbeech.github@mkv25.net>
2018-09-29 20:23:11 +0100

    Use JSON Schema to validate API Endpoints /api/

--- tests/api-endpoints-spec.js
+++ tests/api-endpoints-spec.js
@@ -1,19 +1,14 @@
 const config = require('./helpers/config')
 const { fetch } = require('promise-path')
 const { expect } = require('chai')
+const { validate } = require('jsonschema')
 
 describe('API Endpoints /api/', () => {
   const apiPath = `${config.serverPath}/api/`
+  const apiSchema = require('../api/schemas/endpoints-schema.json')
   it('should display of a list of endpoints', async () => {
     const data = JSON.parse(await fetch(apiPath))
-    expect(data).to.deep.equal({
-      "endpoints": {
-        "/": {
-          "path": "/",
-          "description": "Return a list of endpoints",
-          "accepts": "application/json"
-        }
-      }
-    })
+    const schemaValidation = validate(data, apiSchema)
+    expect(schemaValidation.errors).to.deep.equal([])
   })
 })
