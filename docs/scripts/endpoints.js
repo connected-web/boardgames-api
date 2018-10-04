b86f6b12041f008173de068044b12869e28707d2
John Beech <johnbeech.github@mkv25.net>
2018-10-04 18:23:10 +0100

    Split schema endpoints into separate section

--- docs/scripts/endpoints.js
+++ docs/scripts/endpoints.js
@@ -1,13 +1,20 @@
 $.getJSON('/api/', function(data) {
     data.endpoints = data.endpoints || []
+    const endpoints = data.endpoints.filter(e => !e.path.includes('/schema'))
+    const schemaEndpoints = data.endpoints.filter(e => e.path.includes('/schema'))
     $content = $('<content/>')
-    $.each(data.endpoints, function(key, endpoint) {
+    $content.append('<heading>Endpoints</heading>')
+    $.each(endpoints, renderEndpoint)
+    $content.append('<heading>Schemas</heading>')
+    $.each(schemaEndpoints, renderEndpoint)
+    $('content:first-of-type').append($content)
+
+    function renderEndpoint(key, endpoint) {
       $endpoint = $('<endpoint/>')
       $endpoint.append($('<heading><a href="' + endpoint.path + '">' + endpoint.method + ' ' + endpoint.path + '</a></heading>'))
       $endpoint.append($('<p>' + endpoint.description + '</p>'))
       $endpoint.append($('<pre>Accepts: ' + endpoint.accepts + '</pre>'))
       $endpoint.append($('<p>Schema: <a href="' + endpoint.schema + '">' + endpoint.schema + '</a></p>'))
       $content.append($endpoint)
-    })
-    $('content:first-of-type').append($content)
+    }
 })
