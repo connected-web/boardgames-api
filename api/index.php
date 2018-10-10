517f836a8385f0109a100c746713784f524d8b4f
Markavian <github@mkv25.net>
2018-10-10 22:58:19 +0100

    API /boardgame/stats endpoint (#2)
    * Created new way of defining endpoints based on convention
    * Write up minimum expected properties for /api/boardgame/stats endpoint
    * Update descriptions of generated endpoints
    * Expect endpoints to return asynchronously
    * Expect gamesPlayedPerDay to have numbered day keys presenting integers
    * Specify what types to expect inside nested arrays

--- api/index.php
+++ api/index.php
@@ -7,39 +7,91 @@ if (!isset($_SERVER['HTTPS'])) {
   exit('{"moved": "Redirecting to secure endpoint.", "location": "' . $url .'", "status": "302"}');
 }
 
-$requestUri = $_SERVER['REQUEST_URI'];
-$path = join(array_filter(explode('/', $requestUri)), '/');
-$schema = endsWith($path, 'schema');
-$path = str_replace('api', '', $path);
-$path = $path ? $path : '/';
-
-$endpointsData = json_decode(@file_get_contents('endpoints.json'))->endpoints;
-
-$endpoints = array();
-foreach ($endpointsData as $key=>$value) {
-  $pathKey = $key['path'];
-  $endpoints[$pathKey] = array('data' => $value);
+function registerEndpoints($endpointsData) {
+  $endpoints = array();
+  foreach ($endpointsData as $key => $value) {
+    $pathKey = $value->path;
+    $endpoints[$pathKey] = array('data' => $value);
+  }
+  return $endpoints;
 }
-$endpoints['/']['source'] = 'endpoints.json';
-$endpoints['/schema']['source'] = 'schemas/endpoints-schema.json';
-$endpoints['/sample']['source'] = 'samples/endpoints-sample.json';
-$endpoints['/boardgame/feed']['source'] = '../data/boardgame-feed.json';
-$endpoints['/boardgame/feed/sample']['source'] = 'samples/boardgame-feed-sample.json';
-$endpoints['/boardgame/feed/schema']['source'] = 'schemas/boardgame-feed-schema.json';
 
-if ($endpoints[$path] && $endpoints[$path]['source']) {
-  $source = $endpoints[$path]['source'];
-  $sourceData = json_decode(@file_get_contents($source));
-  exit(json_encode($sourceData));
-} else {
-  header("HTTP/1.0 404 Not Found");
-  exit('{"message":"Resource not found", "status":"404", "path":"' . $path . '"}');
+function defineEndpoint($path, $endpointIndex) {
+  $structure = array(
+    "path" => $path,
+    "method" => "GET",
+    "description" => "",
+    "accepts" => "application/json"
+  );
+
+  if (endsWith($path, "schema")) {
+    $referencePath = str_replace("/schema", "", $path);
+    $structure["description"] = "Returns a JSON schema which can be used to verify $referencePath, and $referencePath/sample";
+    $structure["schema"] = "https://json-schema.org/draft-07/schema";
+  } else if(endsWith($path, "sample")) {
+    $referencePath = str_replace("/schema", "", $path);
+    $structure["description"] = "Returns sample data representative of $referencePath";
+    $structure["schema"] = str_replace("/sample", "/schema", $path);
+  }
+
+  return $structure;
+}
+
+function generateEndpointResponse($requestUri, $endpointIndex, $path) {
+  $result = array('endpoints' => array());
+  foreach ($endpointIndex as $path => $entry) {
+    $data = $entry['data'] ? $entry['data'] : defineEndpoint($path, $endpointIndex);
+    $result['endpoints'][] = $data;
+  }
+  return $result;
+}
+
+function render() {
+  $requestUri = $_SERVER['REQUEST_URI'];
+  $path = '/' . join(array_filter(explode('/', $requestUri)), '/');
+  $schema = endsWith($path, 'schema');
+  $path = $path ? $path : '/api/';
+  $path = $path === '/api' ? '/api/' : $path;
+
+  $endpointsData = json_decode(@file_get_contents('endpoints.json'))->endpoints;
+  $endpoints = registerEndpoints($endpointsData);
+
+  $endpoints['/api/']['source'] = 'generateEndpointResponse';
+  $endpoints['/api/schema']['source'] = 'schemas/endpoints-schema.json';
+  $endpoints['/api/sample']['source'] = 'samples/endpoints-sample.json';
+  $endpoints['/api/boardgame/feed']['source'] = '../data/boardgame-feed.json';
+  $endpoints['/api/boardgame/feed/sample']['source'] = 'samples/boardgame-feed-sample.json';
+  $endpoints['/api/boardgame/feed/schema']['source'] = 'schemas/boardgame-feed-schema.json';
+  $endpoints['/api/boardgame/stats']['source'] = '../data/boardgame-summaries.json';
+  $endpoints['/api/boardgame/stats/sample']['source'] = 'samples/boardgame-stats-sample.json';
+  $endpoints['/api/boardgame/stats/schema']['source'] = 'schemas/boardgame-stats-schema.json';
+
+  if ($endpoints[$path] && $endpoints[$path]['source']) {
+    $source = $endpoints[$path]['source'];
+    if (function_exists($source)) {
+      $body = call_user_func($source, $requestUri, $endpoints, $path);
+      exit(json_encode($body));
+    } else {
+      $sourceData = json_decode(@file_get_contents($source));
+      exit(json_encode($sourceData));
+    }
+  } else {
+    header("HTTP/1.0 404 Not Found");
+    exit('{"message":"Resource not found", "status":"404", "path":"' . $path . '"}');
+  }
+}
+
+function startsWith($haystack, $needle) {
+  $length = strlen($needle);
+  return (substr($haystack, 0, $length) === $needle);
 }
 
 function endsWith($haystack, $needle) {
-    $length = strlen($needle);
-    if ($length == 0) {
-        return true;
-    }
-    return (substr($haystack, -$length) === $needle);
+  $length = strlen($needle);
+  if ($length == 0) {
+    return true;
+  }
+  return (substr($haystack, -$length) === $needle);
 }
+
+render();
--- tests/api-endpoints-spec.js
+++ tests/api-endpoints-spec.js
@@ -38,11 +38,15 @@ describe('API Endpoints', () => {
   it('GET /api/ should list all available endpoints', async () => test(`${config.serverPath}/api/`, `${config.serverPath}/api/schema`))
   it('GET /api/ should match the local data used to generate these tests', async () => {
     const actual = await fetchJSON(`${config.serverPath}/api/`)
-    const expected = endpointData
-    expect(actual).to.deep.equal(expected)
+    const actualEndpoints = actual && actual.endpoints || []
+    endpointData.endpoints.forEach((expectedEndpoint, i) => {
+      expect(actualEndpoints[i]).to.deep.equal(expectedEndpoint)
+    })
   })
 
   endpointData.endpoints.forEach((endpoint) => {
-    it(`${endpoint.method} ${endpoint.path} should '${endpoint.description}'`, () => test(endpoint.path, endpoint.schema))
+    it(`${endpoint.method} ${endpoint.path} should '${endpoint.description}'`, async () => test(endpoint.path, endpoint.schema))
+    it(`${endpoint.method} ${endpoint.path}/schema should provide a valid schema`, async () => test(`${endpoint.path}/schema`, 'https://json-schema.org/draft-07/schema'))
+    it(`${endpoint.method} ${endpoint.path}/sample should provide a valid sample`, async () => test(`${endpoint.path}/sample`, endpoint.schema))
   })
 })
