96989af14fee6473e6949f2ca501949c8ef440c1
John Beech <johnbeech.github@mkv25.net>
2018-10-12 23:17:31 +0100

    Write additional tests around new docs pages

--- tests/home-spec.js
+++ tests/home-spec.js
@@ -2,7 +2,7 @@ const config = require('./helpers/config')
 const { fetch } = require('promise-path')
 const { expect } = require('chai')
 
-describe('Home /', () => {
+describe('/ - Redirect to /docs/', () => {
   it('should redirect to /docs/', async () => {
     const body = await fetch(config.serverPath)
     expect(body).to.contain('<script type="text/javascript">window.location = "/docs/"</script>')
