3f001c8d93883808582ba7ec9df65d6a73590da3
John Beech <johnbeech.github@mkv25.net>
2018-11-14 06:41:03 +0000

    Make sure screenshots render against the correct path

--- tests/docs-screenshot-spec.js
+++ tests/docs-screenshot-spec.js
@@ -6,7 +6,7 @@ const dreamCatcher = require('./helpers/dreamCatcher')
 describe(`Boardgame API Docs Screenshots [${config.name}]`, () => {
   it(`should render a mobile view of the documentation website`, () => {
     return Nightmare({width: 411, height: 731})
-      .goto(`${config.docsUrl}`)
+      .goto(`${config.docsUrl}/`)
       .wait(100)
       .screenshot(screenshotPath('mobile-screen'))
       .end()
@@ -15,7 +15,7 @@ describe(`Boardgame API Docs Screenshots [${config.name}]`, () => {
 
   it(`should render a small screen view of the documentation website`, () => {
     return Nightmare({width: 800, height: 600})
-      .goto(`${config.docsUrl}`)
+      .goto(`${config.docsUrl}/`)
       .wait(100)
       .screenshot(screenshotPath('small-screen'))
       .end()
@@ -24,7 +24,7 @@ describe(`Boardgame API Docs Screenshots [${config.name}]`, () => {
 
   it(`should render a large screen view of the documentation website`, () => {
     return Nightmare({width: 1920, height: 1080})
-      .goto(`${config.docsUrl}`)
+      .goto(`${config.docsUrl}/`)
       .wait(100)
       .screenshot(screenshotPath('large-screen'))
       .end()
