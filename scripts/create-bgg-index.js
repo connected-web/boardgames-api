87bc29266b4c2f8fbf187d441872942d6aba3fe3
John Beech <johnbeech.github@mkv25.net>
2020-03-06 20:22:00 +0000

    Migrate script to use API

--- scripts/create-bgg-index.js
+++ scripts/create-bgg-index.js
@@ -1,24 +1,16 @@
 const { write, position } = require('promise-path')
 const datapath = position(__dirname, '../data')
 const report = (...messages) => console.log('[Create Board Game Geek Index]', ...messages)
+const { model, bggIndex } = require('../')
 
 async function start () {
+  const { boardGameGeek } = await model()
   report('Requires', 'data/bgg-collection.json')
+  boardGameGeek.collection = require(datapath('bgg-collection.json'))
+  const { index } = await bggIndex()
 
-  const collection = require(datapath('bgg-collection.json'))
-  const boardGameGeekIndex = collection.items[0].item.reduce(mapBoardGame, {})
-
-  function mapBoardGame (accumulator, item) {
-    const name = item.name[0]._text[0]
-    const id = item._attributes.objectid
-    accumulator[name] = {
-      boardGameGeekGameId: id
-    }
-    return accumulator
-  }
-
-  report('Index', boardGameGeekIndex)
-  return write(datapath('bgg-index.json'), JSON.stringify(boardGameGeekIndex, null, 2), 'utf8')
+  report('Index', index)
+  return write(datapath('bgg-index.json'), JSON.stringify(index, null, 2), 'utf8')
 }
 
 module.exports = start
