c935f62f9dbe7397b2262218230eb9306b4c3573
John Beech <johnbeech.github@mkv25.net>
2020-03-29 16:01:52 +0100

    Remove sort console.logs

--- src/util/sortByFeedPriority.js
+++ src/util/sortByFeedPriority.js
@@ -3,13 +3,11 @@ function sortByFeedPriority (a, b) {
   const db = (new Date(b.date)).getTime()
   const dateSort = da === db ? 0 : (da > db ? 1 : -1)
   if (dateSort !== 0) {
-    console.log('Sorting by date', a.date, b.date, da, db, a.name, b.name, dateSort)
     return dateSort
   }
 
   const nameSort = (a.name + '').localeCompare((b.name + ''), 'en', {sensitivity: 'base'})
   if (nameSort !== 0) {
-    console.log('Sorting by name', a.date, b.date, a.name, b.name, nameSort)
     return nameSort
   }
   
