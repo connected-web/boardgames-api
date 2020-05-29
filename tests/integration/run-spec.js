a1b58436caa85834741a84524c1c95fa8204de9d
John Beech <john.beech@bbc.co.uk>
2020-05-30 00:52:19 +0100

    Update test with new script

--- tests/integration/run-spec.js
+++ tests/integration/run-spec.js
@@ -4,6 +4,7 @@ const basepath = position(__dirname, '../../')
 
 const expected = `[Board Game API Run] Available scripts to run:
   node run all
+  node run check-issue
   node run create-all
   node run create-bgg-index
   node run create-boardgame-feed
