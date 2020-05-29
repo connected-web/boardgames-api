74472b5682cbd080df98c49d34aa091b9cbcbc22
John Beech <johnbeech@users.noreply.github.com>
2020-05-29 18:44:30 +0100

    #13 Set up automated deployment pipeline (#14)
    - Create a Test & Deploy Workflow
    - Remove Screenshot Tests
    - Start test server up as part of integration tests
    - Remove pre-commit
    - Remove nightmare and tests for docs endpoint

--- tests/integration/helpers/config.js
+++ tests/integration/helpers/config.js
@@ -1,3 +1,4 @@
+const path = require('path')
 const devServerPath = 'http://localhost:4000'
 
 const devEnvironment = {
@@ -5,5 +6,18 @@ const devEnvironment = {
   name: 'Dev',
   docsUrl: `${devServerPath}/docs`
 }
+
+let server
+before(() => {
+  console.log('Opening test server', devServerPath)
+  server = require(path.join(__dirname, '../../../test-server'))
+})
+
+after(() => {
+  console.log('Closing test server', devServerPath)
+  setTimeout(() => {
+    server.close()
+  }, 50)
+})
 
 module.exports = devEnvironment
