40ca74c93a00f9051f86f76cb13d55e7f8379f45
John Beech <johnbeech.github@mkv25.net>
2020-05-30 21:44:35 +0100

    Add check to match and proceed against correct issue title

--- scripts/check-issue.js
+++ scripts/check-issue.js
@@ -1,14 +1,18 @@
 const report = (...messages) => console.log('[Check Issue]', ...messages)
 
 const issueId = process.argv[3]
-const issueTitle = process.argv[4]
-const expectedIssueTitle = process.argv[5]
+const issueTitle = (process.argv[4] + '').trim()
+const expectedIssueTitle = (process.argv[5] + '').trim()
 
 async function start () {
-  report('Issue Id', issueId)
-  report('Issue Title', issueTitle)
-  report('Expected Issue Title', expectedIssueTitle)
-  process.exit(1)
+  if (issueTitle === expectedIssueTitle) {
+    report('Matched', issueId, expectedIssueTitle)
+    process.exit(0)
+  }
+  else {
+    report('Did not match', issueId, issueTitle)
+    process.exit(1)
+  }
 }
 
 module.exports = start
