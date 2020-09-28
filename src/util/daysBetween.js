40be32c3c4e5e333da6011b1f138f9d3827b4f57
John Beech <johnbeech.github@mkv25.net>
2020-09-28 21:50:27 +0100

    Try and fix daysBetween calculation to ignore hours/minutes/seconds

--- src/util/daysBetween.js
+++ src/util/daysBetween.js
@@ -1,7 +1,10 @@
 function daysBetween (firstDate, secondDate) {
+  const flatStart = new Date(firstDate.toISOString().substring(0, 10))
+  const flatEnd = new Date(secondDate.toISOString().substring(0, 10))
+
   const oneDayInMilliseconds = 24 * 60 * 60 * 1000
-  const firstTime = firstDate.getTime()
-  const secondTime = secondDate.getTime()
+  const firstTime = flatStart.getTime()
+  const secondTime = flatEnd.getTime()
   const millisecondsBetween = Math.abs(firstTime - secondTime)
   return Math.round(millisecondsBetween / oneDayInMilliseconds) + 1
 }
