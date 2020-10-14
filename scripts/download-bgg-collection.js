2288bd5dbd7d01c8eef81d7c4325d4878cb42001
John Beech <johnbeech.github@mkv25.net>
2020-10-14 21:18:46 +0100

    Only replace bgg-collection.json if received data is large enough

--- scripts/download-bgg-collection.js
+++ scripts/download-bgg-collection.js
@@ -9,7 +9,13 @@ async function start () {
     const body = JSON.stringify(collection, null, 2)
     const filename = 'bgg-collection.json'
     report('Downloaded', body.length, 'bytes; writing to:', filename)
-    return write(datapath(filename), body, 'utf8')
+    if (body.length > 100000) {
+      return write(datapath(filename), body, 'utf8')
+    }
+    else {
+      report('Not writing file; received body too small compared to expected data.', body + '')
+      return false
+    }
   } catch (ex) {
     report('Error fetching data:', ex, ex.stack)
   }
