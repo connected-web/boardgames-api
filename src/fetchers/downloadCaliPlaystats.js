511453a8be2431b5b3cfcfb635446e94fcfe0848
John Beech <johnbeech.github@mkv25.net>
2021-02-21 11:48:46 +0000

    Add more sensitive logging around downloading play stats

--- src/fetchers/downloadCaliPlaystats.js
+++ src/fetchers/downloadCaliPlaystats.js
@@ -1,28 +1,39 @@
 const readCredentials = require('../util/readCredentials')
-const spreadsheetId2018 = '1WUx5D5gONHukgaHjqLl334fBUQzy6NQiaEouztVp-L4'
-const spreadsheetId2020 = '1FyeEeWPKch4jVoBoD_lB89GbsUPkYTESkfIeknUhyJw'
-const spreadsheetId2021 = '1Va-06sfmVq-UFBxWwo6U5v6N9W_lizW5GvgyogQdGQQ'
-const log = []
-const report = (...messages) => log.push(['[Download Cali Play Stats]', ...messages].join(' '))
+const spreadsheets = [{
+  year: 2018,
+  id: '1WUx5D5gONHukgaHjqLl334fBUQzy6NQiaEouztVp-L4'
+}, {
+  year: 2020,
+  id: '1FyeEeWPKch4jVoBoD_lB89GbsUPkYTESkfIeknUhyJw'
+}, {
+  year: 2021,
+  id: '1Va-06sfmVq-UFBxWwo6U5v6N9W_lizW5GvgyogQdGQQ'
+}]
 
-async function downloadData ({ gsjson }, spreadsheetId) {
+const log = []
+const report = (...messages) => console.log(['[Download Cali Play Stats]', ...messages].join(' '))
+
+async function downloadData ({ gsjson }, { year, id }) {
+  report('Downloading data for', year, 'using id:', id)
+  const spreadsheetId = id
   try {
     const credentials = await readCredentials()
     const worksheets = await gsjson({ spreadsheetId, allWorksheets: true, credentials })
     report('Downloaded data:', (worksheets + '').length, 'bytes')
-    const entries = worksheets.reduce((acc, item) => acc.concat(item), [])
-    return entries
+    const cells = worksheets.reduce((acc, item) => acc.concat(item), [])
+    report(year, 'cells', cells.length)
+    return cells
   } catch (ex) {
     report('Unable to download data:', ex)
   }
 }
 
 async function updatePlaystats (model) {
-  const combinedData = await Promise.all([
-    downloadData(model.fetchers, spreadsheetId2018),
-    downloadData(model.fetchers, spreadsheetId2020),
-    downloadData(model.fetchers, spreadsheetId2021)
-  ])
+  const { fetchers } = model
+  const asyncWork = spreadsheets.map(spreadsheet => {
+    return downloadData(fetchers, spreadsheet)
+  })
+  const combinedData = await Promise.all(asyncWork)
 
   const playStats = combinedData.reduce((acc, item) => acc.concat(item), [])
   return playStats.filter(n => n)
